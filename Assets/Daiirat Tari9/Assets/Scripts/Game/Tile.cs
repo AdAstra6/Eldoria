98d3eead1cd3d805773d4ea21991ee1b34e14775
Belloul Wassim Zine eddine <nw_belloul@esi.dz>
2025-03-25 18:26:42 +0100

    Tile Types introduced

--- Assets/Daiirat Tari9/Assets/Scripts/Game/Tile.cs
+++ Assets/Daiirat Tari9/Assets/Scripts/Game/Tile.cs
@@ -3,8 +3,45 @@ using System.Collections.Generic;
 
 public class Tile : MonoBehaviour
 {
+    public enum TileType { Normal, Question, Bonus, Penalty, Teleport }
+    public TileType type;
+    
     public List<Tile> nextTiles; // List of possible paths from this tile
-    public bool isCrossway = false; // Mark if this is a crossway
+    public bool isCrossway = false; // Crossway is handled separately
+
+    public void TriggerEffect(Player player)
+    {
+        switch (type)
+        {
+            case TileType.Question:
+                Debug.Log(player.name + " landed on a Question tile!");
+                // QuestionManager.Instance.AskQuestion(player);
+                break;
+
+            case TileType.Bonus:
+                Debug.Log(player.name + " landed on a Bonus tile!");
+                // player.GainReward();
+                GameManager.Instance.EndTurn();
+                break;
+
+            case TileType.Penalty:
+                Debug.Log(player.name + " landed on a Penalty tile!");
+                // player.ReceivePenalty();
+                GameManager.Instance.EndTurn();
+                break;
+
+            case TileType.Teleport:
+                Debug.Log(player.name + " landed on a Teleport tile!");
+                // player.TeleportToRandomTile();
+                GameManager.Instance.EndTurn();
+                break;
+
+            default:
+                Debug.Log(player.name + " landed on a Normal tile.");
+                GameManager.Instance.EndTurn();
+                break;
+        }
+    }
 
     public Tile GetNextTile()
     {
