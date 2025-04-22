95d521399efb7f7e5818159c15392d710ef67e83
Oussama <oussamammm7@gmail.com>
2025-04-22 11:16:12 +0100

    profile book flip animation fixed

--- Assets/Scripts/ProfilePanel/ProfilePanelManager.cs
+++ Assets/Scripts/ProfilePanel/ProfilePanelManager.cs
@@ -1,28 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class ProfilePanelManager : MonoBehaviour
 {
     [SerializeField] private Animator bookAnimator;
     [SerializeField] private ProfilePanelUImanager uimanager;
+    private float delayTime = 1.1f;  // Delay time for the fade animation when flipping the page
+    List<PlayerProfile> profiles;
+    int currentProfileIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ProfileManager profileManager = new ProfileManager();
+        profiles = profileManager.LoadProfiles();
+        bool hasProfiles = profiles.Count > 0;
+        if (hasProfiles)
+        {
+            uimanager.UpdateProfileDisplay(profiles[currentProfileIndex]);
+            uimanager.HideErrorText();
+        }
+        else
+        {
+            uimanager.SetErrorText();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    public void NextProfile()
+    }
+    // Wrapper method with void return type
+    public void StartNextProfile()
+    {
+        StartCoroutine(NextProfile());
+    }
+    public IEnumerator NextProfile()
     {
         // update profiles
         uimanager.startStatsFadeOut();
+        currentProfileIndex++;
+        if (currentProfileIndex >= profiles.Count)
+        {
+            currentProfileIndex = 0;
+        }
         bookAnimator.SetTrigger("RightFlipPage");
+        yield return new WaitForSeconds(delayTime);
         uimanager.startStatsFadeIn();
+        uimanager.UpdateProfileDisplay(profiles[currentProfileIndex]);
     }
 }
--- Assets/Scripts/ProfilePanel/ProfilePanelUImanager.cs
+++ Assets/Scripts/ProfilePanel/ProfilePanelUImanager.cs
@@ -1,29 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ProfilePanelUImanager : MonoBehaviour
 {
     [SerializeField] FadeScript statsFadeIn;
     [SerializeField] BookPanel Book;
+
+
+    public Image avatar;
+    public TMP_Text usernameText;
+    public TMP_Text idText;
+    public TMP_Text eloText;
+    public TMP_Text totalGamesText;
+    public TMP_Text totalWonsText;
+    public TMP_Text errorText;
+
+    public Button prevButton;
+    public Button nextButton;
+
+
     void Start()
     {
-       
-       Book.StartStatsFadeIn += () => startStatsFadeIn();
+        Book.StartStatsFadeIn += () => startStatsFadeIn();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame  
     void Update()
     {
-        
+
     }
     public void startStatsFadeIn()
     {
         statsFadeIn.StartFadeIn();
-        
+
     }
     public void startStatsFadeOut()
     {
         statsFadeIn.StartFadeOut();
     }
+
+    public void UpdateProfileDisplay(PlayerProfile p)
+    {
+
+
+
+        if (usernameText) usernameText.text = p.Name;
+        if (idText) idText.text = $"{p.Id}";
+        if (eloText) eloText.text = p.Elo.ToString();
+        if (totalGamesText) totalGamesText.text = p.Games.Played.ToString();
+        if (totalWonsText) totalWonsText.text = p.Games.Won.ToString();
+
+        // Avatar loading  
+        if (avatar != null)
+        {
+            Sprite avatarSprite = Resources.Load<Sprite>(p.Icon);
+            if (avatarSprite != null)
+                avatar.sprite = avatarSprite;
+            else
+                Debug.LogWarning($"Avatar sprite not found for icon: {p.Icon}");
+        }
+
+        // TODO : Update stars  
+    }
+
+    public void SetErrorText()
+    {
+        if (errorText != null)
+        {
+            errorText.text = "No profiles found";
+            errorText.gameObject.SetActive(true);
+        }
+    }
+    public void HideErrorText()
+    {
+        if (errorText != null)
+        {
+            errorText.gameObject.SetActive(false);
+        }
+    }
+
 }
