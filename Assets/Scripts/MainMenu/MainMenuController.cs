35d794869389d743c456d5c02465668f9b0d32b1
AnesBhm <anesbouhoum8@gmail.com>
2025-04-27 11:49:55 +0100

    yal moraba3333 mergitlk l mainnnn bel menu

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -3,13 +3,46 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject MainMenuPanel;
+    public GameObject SubMenuPanel;
+    public GameObject TutorialPanel;
+    public GameObject CreditsPanel;
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
     public void PlayGame()
     {
-        SceneManager.LoadScene("CreateGame"); 
+        SceneManager.LoadScene("CreateGame");
 
     }
     public void optionsPanel()
     {
         SceneManager.LoadScene("Options");
+    }
+    public void ShowMainMenu()
+    {
+        MainMenuPanel.SetActive(true);
+        SubMenuPanel.SetActive(false);
+    }
+    public void ShowSubMenu()
+    {
+        MainMenuPanel.SetActive(false);
+        SubMenuPanel.SetActive(true);
+        TutorialPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+    }
+    public void ShowTutorial()
+    {
+        SubMenuPanel.SetActive(false);
+        TutorialPanel.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        SubMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
     }
 }
