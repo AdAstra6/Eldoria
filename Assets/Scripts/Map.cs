8c0197260c5540f0593c3dc1244a959ed517681b
Oussama <oussamammm7@gmail.com>
2025-05-05 01:55:47 +0100

    integrated the 5 bioms map- need tests

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    public const int spawnpointsCount=6;
+    public const int spawnpointsCount=10;
     [SerializeField] private List<Tile> spawnPoints;
     private void Start()
     {
@@ -23,12 +23,13 @@ public class Map : MonoBehaviour
         List<Tile> result = new List<Tile>();
         int index = Random.Range(0, spawnpointsCount);
         int step = spawnpointsCount / n;
+        if (spawnpointsCount % n != 0) step++;
         for (int i = 0; i < n; i++)
         {
             Tile tile = spawnPoints[index];
             tile.isSpawnPoint = true; // ✅ Mark as spawn tile
             result.Add(tile);
-            index = (index + Random.Range(1, step)) % Map.spawnpointsCount;
+            index = (index + Random.Range(2, step)) % Map.spawnpointsCount;
         }
         return result;
     }
